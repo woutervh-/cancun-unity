a2fcf9d172cabf6f843dd1343e4c6047dc16f973
Wouter van Heeswijk <woutervh@gmail.com>
2017-12-19 22:22:43 +0100

    Globe and text

---
+++ Assets/EarthTextures.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthTextures : MonoBehaviour
+{
+    private string url = "https://api.tomtom.com/map/1/tile/basic/main/0/0/0.png?key=zu3b4a3g2x87pc2sdqeb6put";
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        Texture2D tex = new Texture2D(256, 256, TextureFormat.RGB24, false);
+        tex.filterMode = FilterMode.Trilinear;
+        tex.anisoLevel = 9;
+        WWW www = new WWW(url);
+        yield return www;
+        www.LoadImageIntoTexture(tex);
+        GetComponent<Renderer>().material.mainTexture = tex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/RotateAboutCamera.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateAboutCamera : MonoBehaviour
+{
+    public Text latitudeText;
+    public Text longitudeText;
+    public float speedHorizontal = 180.0f;
+    public float speedVertical = 90.0f;
+    public float radius = 2.0f;
+    
+    private Vector3 dragOrigin = Vector3.zero;
+    private bool isDragging = false;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    
+    void Start()
+    {
+        UpdateValues();
+    }
+    
+    void LateUpdate()
+    {
+        if (!isDragging && Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            isDragging = true;
+        }
+        else if (isDragging && Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        if (isDragging)
+        {
+            Vector3 position = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            dragOrigin = Input.mousePosition;
+            yaw += position.x * speedHorizontal;
+            pitch += -position.y * speedVertical;
+
+            UpdateValues();
+        }
+    }
+
+    void UpdateValues()
+    {
+        transform.position = CalculatePosition();
+        transform.rotation = CalculateRotation();
+        latitudeText.text = "Latitude: " + pitch;
+        longitudeText.text = "Longitude: " + yaw;
+    }
+
+    Vector3 CalculatePosition()
+    {
+        return CalculateRotation() * Vector3.back * radius;
+    }
+
+    Quaternion CalculateRotation()
+    {
+        return Quaternion.Euler(pitch, yaw, 0);
+    }
+}

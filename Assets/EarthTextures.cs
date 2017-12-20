6ccade34d32b922e8cac1dfd042a7139257ebce6
Wouter van Heeswijk <woutervh@gmail.com>
2017-12-20 22:18:01 +0100

    Added zoomLevel and tiledNeeded values

--- Assets/EarthTextures.cs
+++ Assets/EarthTextures.cs
@@ -7,12 +7,14 @@ public class EarthTextures : MonoBehaviour
 {
     public Text areaText;
 
+    private static int tileSize = 256;
+
     private string url = "https://api.tomtom.com/map/1/tile/basic/main/0/0/0.png?key=zu3b4a3g2x87pc2sdqeb6put";
 
     // Use this for initialization
     IEnumerator Start()
     {
-        Texture2D tex = new Texture2D(256, 256, TextureFormat.RGB24, false);
+        Texture2D tex = new Texture2D(tileSize, tileSize, TextureFormat.RGB24, false);
         tex.filterMode = FilterMode.Trilinear;
         tex.anisoLevel = 9;
         WWW www = new WWW(url);
@@ -27,7 +29,9 @@ public class EarthTextures : MonoBehaviour
         float unitPixelRadius = (Camera.main.projectionMatrix * Vector3.right).x * Camera.main.pixelWidth;
         float earthPixelRadius = unitPixelRadius / 2;
         float earthPixelArea = Mathf.PI * earthPixelRadius * earthPixelRadius;
+        int tilesNeeded = Mathf.CeilToInt(earthPixelArea / (tileSize * tileSize));
+        float zoomLevel = Mathf.Log(tilesNeeded, 2);
 
-        areaText.text = "Area: " + earthPixelArea;
+        areaText.text = "Area: " + earthPixelArea + "p² (tiles: " + tilesNeeded + ", zoom level: " + zoomLevel + ")";
     }
 }
